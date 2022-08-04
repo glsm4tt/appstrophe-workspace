f4d42b98ceb41dd1a538ea2bad30643109a4bc85
sdedieu <dedieu.sylvain38@gmail.com>
2022-08-04 19:23:18 +0200

    fix husky pre-commit

--- apps/auth/src/app/app.module.ts
+++ apps/auth/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { environment } from '../environments/environment';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
     RouterModule.forRoot([
       {
         path: 'auth',
