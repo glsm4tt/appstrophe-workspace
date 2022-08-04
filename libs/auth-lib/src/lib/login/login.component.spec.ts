d2d78c63bff14597ccc0609ae05b2f311c94afcc
sdedieu <dedieu.sylvain38@gmail.com>
2022-08-04 17:38:27 +0200

    Use the v7 api of firebase

--- libs/auth-lib/src/lib/login/login.component.spec.ts
+++ libs/auth-lib/src/lib/login/login.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { AngularFireModule } from '@angular/fire/compat';
 import { ValidationErrors } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
+import { AuthService } from '../services/auth.service';
 
 import { LoginComponent } from './login.component';
 
@@ -13,9 +13,11 @@ describe('LoginComponent', () => {
     await TestBed.configureTestingModule({
       imports: [
         LoginComponent, 
-        RouterTestingModule, 
-        AngularFireModule.initializeApp({})
+        RouterTestingModule
       ],
+      providers: [
+        {provide: AuthService, useValue: {}}
+      ]
     }).compileComponents();
   });
 
