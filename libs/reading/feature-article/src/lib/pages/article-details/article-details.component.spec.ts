7b446d86f627fc2b507a09cb3b9a8caf32fe4b44
sdedieu <dedieu.sylvain38@gmail.com>
2022-08-10 18:03:11 +0200

    Add Unit tests on comments features (part 2)

--- libs/reading/feature-article/src/lib/pages/article-details/article-details.component.spec.ts
+++ libs/reading/feature-article/src/lib/pages/article-details/article-details.component.spec.ts
@@ -1,42 +1,67 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { provideAuth, getAuth } from '@angular/fire/auth';
-import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { RouterTestingModule } from '@angular/router/testing';
 import { MarkdownModule } from 'ngx-markdown';
 
 import { ArticleDetailsComponent } from './article-details.component';
 import { AuthService } from '@appstrophe-workspace/auth-lib';
 import { EffectsModule } from '@ngrx/effects';
-import { StoreModule } from '@ngrx/store';
+import { Store, StoreModule } from '@ngrx/store';
+import { ActivatedRoute } from '@angular/router';
+
+import * as fromArticle from '@appstrophe-workspace/reading/domain';
+
+const ARTICLE_ID = '4';
 
 describe('ArticleDetailsComponent', () => {
   let component: ArticleDetailsComponent;
   let fixture: ComponentFixture<ArticleDetailsComponent>;
+  let store: Store;
+  let route: ActivatedRoute;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        ArticleDetailsComponent, 
-        HttpClientModule, 
-        MarkdownModule.forRoot({loader: HttpClient}),
+        ArticleDetailsComponent,
+        HttpClientModule,
+        MarkdownModule.forRoot({ loader: HttpClient }),
         RouterTestingModule,
         StoreModule.forRoot({}),
         EffectsModule.forRoot([])
       ],
       providers: [
-        {provide: AuthService, useValue: {}}
+        { provide: AuthService, useValue: {} },
+        {
+          provide: ActivatedRoute, useValue: {
+            snapshot: {
+              params: {
+                articleId: ARTICLE_ID
+              }
+            }
+          }
+        }
       ]
     }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ArticleDetailsComponent);
+    store = TestBed.inject(Store);
+    route = TestBed.inject(ActivatedRoute);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should dispatch the load article action with the route parameter during the init', () => {
+    const spy = jest.spyOn(store, 'dispatch');
+
+    component.ngOnInit();
+
+    expect(spy).toBeCalledTimes(1);
+    expect(spy).toBeCalledWith(fromArticle.loadArticle({ articleId: ARTICLE_ID }))
   });
 });
