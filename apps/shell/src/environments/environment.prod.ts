36b68fefe08283cabf1bb355d94a292ec62e515f
Sylvain Dedieu <sylvaindedieu@Sylvains-MacBook-Pro.local>
2023-02-08 17:22:42 +0100

    Retrieve ignored files

---
+++ apps/shell/src/environments/environment.prod.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDA52qapBEX_PuB0o72238ABwiayDB2F7g",
+    authDomain: "appstrophe-test.firebaseapp.com",
+    projectId: "appstrophe-test",
+    storageBucket: "appstrophe-test.appspot.com",
+    messagingSenderId: "735948884316",
+    appId: "1:735948884316:web:b231733b3dd7e252cb9999",
+    measurementId: "G-8NE5ZG27G5"
+  },
+  host: {
+    auth: 'https://appstrophe-auth.web.app/remoteEntry.js',
+    blog: 'https://appstrophe-blog.web.app/remoteEntry.js'
+  }
+};
