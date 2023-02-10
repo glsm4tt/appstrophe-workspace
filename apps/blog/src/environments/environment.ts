c3ad75a5bacd4a9f16e228146990e59c80c8f438
Sylvain Dedieu <sylvaindedieu@Sylvains-MacBook-Pro.local>
2023-02-10 18:37:55 +0100

    env

--- apps/blog/src/environments/environment.ts
+++ apps/blog/src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyDA52qapBEX_PuB0o72238ABwiayDB2F7g",
-    authDomain: "appstrophe-test.firebaseapp.com",
-    projectId: "appstrophe-test",
-    storageBucket: "appstrophe-test.appspot.com",
-    messagingSenderId: "735948884316",
-    appId: "1:735948884316:web:b231733b3dd7e252cb9999",
-    measurementId: "G-8NE5ZG27G5"
+    apiKey: "__API_KEY__",
+    authDomain: "__AUTH_DOMAIN__",
+    projectId: "__PROJECT_ID__",
+    storageBucket: "__STORAGE_BUCKET__",
+    messagingSenderId: "__MASSAGING_SENDER_ID__",
+    appId: "__APP_ID__",
+    measurementId: "__MEASUREMENT_ID__"
   }
 };
 
