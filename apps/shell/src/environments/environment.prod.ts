3517aa7786080fb484b6dd44d2226fc771ead823
Sylvain Dedieu <sylvaindedieu@Sylvains-MacBook-Pro.local>
2023-02-10 18:21:55 +0100

    fix(env): Push mocked envs

---
+++ apps/shell/src/environments/environment.prod.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "__API_KEY__",
+    authDomain: "__AUTH_DOMAIN__",
+    projectId: "__PROJECT_ID__",
+    storageBucket: "__STORAGE_BUCKET__",
+    messagingSenderId: "__MASSAGING_SENDER_ID__",
+    appId: "__APP_ID__",
+    measurementId: "__MEASUREMENT_ID__"
+  },
+  host: {
+    auth: 'https://appstrophe-auth-f3e74.web.app/remoteEntry.js',
+    blog: 'https://appstrophe-blog-46f62.web.app/remoteEntry.js'
+  }
+};
