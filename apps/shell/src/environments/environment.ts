bc65d380dba58177a2e7d8a2b9f3beb048bf0164
glsm4tt <41773294+glsm4tt@users.noreply.github.com>
2023-02-19 09:21:06 +0100

    Create environment.ts

---
+++ apps/shell/src/environments/environment.ts
@@ -0,0 +1,16 @@
+export const environment = {
+    production: false,
+    firebase: {
+      apiKey: "__API_KEY__",
+      authDomain: "__AUTH_DOMAIN__",
+      projectId: "__PROJECT_ID__",
+      storageBucket: "__STORAGE_BUCKET__",
+      messagingSenderId: "__MASSAGING_SENDER_ID__",
+      appId: "__APP_ID__",
+      measurementId: "__MEASUREMENT_ID__"
+    },
+    host: {
+      auth: 'http://localhost:4201/remoteEntry.js',
+      blog: 'http://localhost:4202/remoteEntry.js'
+    }
+  };
