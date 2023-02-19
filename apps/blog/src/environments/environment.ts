81d06422643415300e038d5f9097fcefd2468e42
glsm4tt <41773294+glsm4tt@users.noreply.github.com>
2023-02-19 09:20:32 +0100

    Create environment.ts

---
+++ apps/blog/src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    firebase: {
+      apiKey: "__API_KEY__",
+      authDomain: "__AUTH_DOMAIN__",
+      projectId: "__PROJECT_ID__",
+      storageBucket: "__STORAGE_BUCKET__",
+      messagingSenderId: "__MASSAGING_SENDER_ID__",
+      appId: "__APP_ID__",
+      measurementId: "__MEASUREMENT_ID__"
+    }
+  };
+  
