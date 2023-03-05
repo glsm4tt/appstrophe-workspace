9d49c20282175a4a9a10736c169f03fd35fb611a
Sylvain Dedieu <sylvaindedieu@Sylvains-MacBook-Pro.local>
2023-03-05 23:43:39 +0100

    feat(auth): Add the triggered cloud function that automatically remove the firestore user when the auth user account is deleted

--- functions/test/test.online.ts
+++ functions/test/test.online.ts
@@ -9,12 +9,14 @@ process.env.FIRESTORE_EMULATOR_HOST = "localhost:8080"
 const test = firebaseFunctionsTest({projectId: firebaseConfig.projectId}, "./serviceAccountKey.json");
 
 import * as functions from "../src/index";
+import { UserMetadata, UserRecord } from "firebase-admin/auth";
 
 const db = admin.firestore();
 
 describe("appstrophe cloud functions api", () => {
     const articleId = "article_1";
     const commentId = "lFWv2p7kheOkCstn8sRE";
+    const reactionId = "5cPUb6FzyMjmoanEOvE66zbUxzAK";
 
     it("should add the author and the date to the comment when onCommentCreate is called", async () => {
         
@@ -42,5 +44,51 @@ describe("appstrophe cloud functions api", () => {
         assert.deepEqual(data?.author, expected.author);
         assert.equal(data?.text, expected.text);
         assert.isBelow(data?.date.seconds - expected.date.seconds, 5);
-    })
+    });
+
+    it("should add the date to the reaction when onCommentLikeCreate is called", async () => {
+        
+        const expected = {
+            date: admin.firestore.Timestamp.now(),
+            type: "like"
+        };
+
+        const wrapped = test.wrap(functions.onCommentLikeCreate);
+        await wrapped({type: "like"}, {
+            auth: {
+                uid: "Ki3WFGMtnDGTHB0ArzV0XNbNyOt5"
+            },
+            params: {
+                articleId, 
+                commentId,
+                reactionId
+            }
+        })
+        const docRef = await db.doc(`articles/${articleId}/comments/${commentId}/reactions/${reactionId}`).get();
+        const data = docRef.data();
+        assert.equal(data?.type, expected.type);
+        assert.isBelow(data?.date.seconds - expected.date.seconds, 5);
+    });
+
+    it("should delete the user firestore document when the auth user is deleted", async () => {
+        const userRecord: UserRecord = {
+            uid: "Ki3WFGMtnDGTHB0ArzV0XNbNyOt5",
+            emailVerified: false,
+            disabled: false,
+            metadata: {} as UserMetadata,
+            providerData: [],
+            toJSON: function (): object {
+                throw new Error("Function not implemented.");
+            }
+        }
+        
+        let userRef = await db.doc(`users/${userRecord.uid}`).get();
+
+        assert.equal(userRef?.id, userRecord.uid);
+        
+        const wrapped = test.wrap(functions.onUserDeleted);
+        await wrapped(userRef);
+        userRef = await db.doc(`user/${userRecord.uid}`).get();
+        assert.isFalse(userRef.exists);
+    });
 });
